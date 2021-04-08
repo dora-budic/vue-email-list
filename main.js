b4d3e00ec9057538c2e78e2cf6f11d74d0a320dd
Dora Budić <dbudic0706@gmail.com>
2021-04-08 16:03:35 +0200

    Bonus

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 var app = new Vue({
   el: '#root',
   data: {
+    emails: [],
   },
-  methods: {
-    email: function () {
-      for (var i = 0; i < 10; i++) {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-          .then((response) => {
-            document.getElementById('email-list').innerHTML += `<li>${response.data.response}</li>`;
-          });
-      }
+  mounted () {
+    for (var i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+          this.emails.push(response.data.response);
+        });
     }
-  }
+  },
 });
